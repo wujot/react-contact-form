51df194f253594546d27c664b13498c7eda8394f
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-02-21 19:03:17 +0100

    Contacts component changed

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,20 +1,13 @@
 var Contacts = React.createClass({
   propTypes: {
-    items: React.PropTypes.array.isRequired,
+    items: React.PropTypes.array.isRequired
   },
 
-  render: function() {
+  render() {
     var contacts = this.props.items.map(function(contact) {
-        return React.createElement(Contact, {item: contact, key: contact.id});
+      return <Contact item={contact} key={contact.id} />;
     });
 
-    return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
-      /*
-      <ul className={'contactsList'}>
-        <Contact item={contact} key={contact.id} />
-      </ul>
-      */
-    );
+    return <ul>{contacts}</ul>;
   }
 });
